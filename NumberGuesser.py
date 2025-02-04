00b851a12d6d76f5502203c0c9a68981308183bd
Viral Sakdecha <81472120+ViralSakdecha@users.noreply.github.com>
2025-02-04 22:17:22 +0530

    Add files via upload

---
+++ NumberGuesser.py
@@ -0,0 +1,25 @@
+import random
+
+print("Welcome to random Number Generator:")
+
+num = random.randrange(100)
+chance = 7
+
+print("You have 7 chances to guess the number:")
+
+for i in range(0,chance,1):
+    
+    guess = int(input("Guess number between 1 to 100:"))
+    
+    if num==guess:
+        print("Congrats!, You guessed the number in",i+1,attemps)
+        
+    elif num>guess:
+        print("Guess is lower")  
+   
+    elif num<guess:
+        print("Guess is higher")
+        
+        
+else:
+    print("Number was:",num,"Better Luck Next Time") 
